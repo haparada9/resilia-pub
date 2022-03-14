848752fdec9e540d103978b39c4858cc534c3c4f
Hugo de Assis Parada <ha.parada9@gmail.com>
2022-03-14 14:34:32 -0300

    adicionando app.js

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+//import routes from './routes/index.js';
+
+const app = express();
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+const port = 3000
+
+app.listen(port, () => console.log(`Servidor funcionando na porta ${port}`))
+
+routes(app);
