757683e7aeda4f96110e5d223baba8a4a4b1d291
Hugo de Assis Parada <ha.parada9@gmail.com>
2022-03-14 14:38:33 -0300

    adicionando Routes

---
+++ src/routes/clientesRoute.js
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+import ClientesController from '../controller/ClientesController.js';
+
+const router = Router();
+const clientes = new ClientesController()
+
+router.post("/criarTabela", clientes.criarTabela)
+
+router.get("/clientes", clientes.buscarTodosClientes)
+
+router.post("/clientes", clientes.salvarClientes)
+
+export default router;
---
+++ src/routes/index.js
@@ -0,0 +1,5 @@
+import clientes from './clientesRoute.js'
+
+export default (app) => {
+   app.use(clientes)
+}
