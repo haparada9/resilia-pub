a3106276aaa67abef5d63f8953ea371518bb616b
Hugo de Assis Parada <ha.parada9@gmail.com>
2022-03-17 16:07:57 -0300

    implementando o método criar cliente.

--- src/DAO/MetodosClientes.js
+++ src/DAO/MetodosClientes.js
@@ -1,12 +1,11 @@
-import { bdClientes} from '../model/cliente.js';
+import { bdClientes } from "../model/cliente.js";
 
-class MetodosClientes
-
-//Método create.
-criarTabela(){
-    try{
-        return new Promise((resolve, reject)=>{
-            const scriptCriarTabela = `
+class MetodosClientes {
+  //Método create.
+  criarTabela() {
+    try {
+      return new Promise((resolve, reject) => {
+        const scriptCriarTabela = `
             CREATE TABLE IF NOT EXISTS clientes (
                 id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
                 nome VARCHAR(50),
@@ -14,17 +13,41 @@ criarTabela(){
                 data_nascimento DATE,
                 cpf INTEGER,
                 telefone VARCHAR(20)
-            )`
+            )`;
 
-        bdClientes.run(scriptCriarTabela, (e)=>{
-            if(!e){
-                resolve("Tabela criada com sucesso.")
-            } else{
-                reject("Erro ao criar tabela.")
-            }
-        })
-        })
-    } catch(e){
-        console.error(e.message)
+        bdClientes.run(scriptCriarTabela, (e) => {
+          if (!e) {
+            resolve("Tabela criada com sucesso.");
+          } else {
+            reject("Erro ao criar tabela.");
+          }
+        });
+      });
+    } catch (e) {
+      console.error(e.message);
     }
+  }
+
+  //Método Create
+  criarCliente(nome, genero, data_nascimento, cpf, telefone) {
+    try {
+      return new Promise((resolve, reject) => {
+        const scriptInsertCliente = `
+            INSERT INTO clientes (nome, genero, data_nascimento, cpf, telefone) VALUES
+            ('${nome}', '${genero}', ${data_nascimento}, ${cpf}, ${telefone})
+        `;
+        bdClientes.run(scriptInsertCliente, (e) => {
+          if (!e) {
+            resolve(`Dados do cliente
+                nome: ${nome}
+                inserido com sucesso`);
+          } else {
+            reject("Erro ao salvar dados do Cliente.");
+          }
+        });
+      });
+    } catch (e) {
+      console.log(e.message);
+    }
+  }
 }
