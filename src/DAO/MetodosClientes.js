462a5b99d264450de601bad6e2d998d9b8e9f965
Hugo de Assis Parada <ha.parada9@gmail.com>
2022-03-17 16:45:54 -0300

    implementando o método de atualizar cliente

--- src/DAO/MetodosClientes.js
+++ src/DAO/MetodosClientes.js
@@ -50,62 +50,89 @@ class MetodosClientes {
       console.log(e.message);
     }
   }
+
+  //Método Read
+  async buscarTodosClientes() {
+    try {
+      return new Promise((resolve, reject) => {
+        const scriptSelectClientes = `SELECT * FROM clientes`;
+        bdClientes.all(scriptSelectClientes, (e, rows) => {
+          if (!e) {
+            resolve(rows);
+          } else {
+            reject("Problema ao obter dados");
+          }
+        });
+      });
+    } catch (e) {
+      console.error(e.message);
+    }
+  }
+
+  //Método Read
+  getClientesId(id) {
+    try {
+      return new Promise((resolve, reject) => {
+        const scriptSelectClienteId = `SELECT * FROM clientes WHERE id = ${id}`;
+        bdClientes.get(scriptSelectClienteId, (e, row) => {
+          if (!e) {
+            if (row != undefined) {
+              resolve(row);
+            } else {
+              reject("Nenhum registro encontrado");
+            }
+          } else {
+            reject("Não foi possível acessar o banco de dados");
+          }
+        });
+      });
+    } catch (e) {
+      console.error(e.message);
+    }
+  }
+
+  //Método delete
+  deleteCliente(id) {
+    try {
+      return new Promise((resolve, reject) => {
+        const scriptDelete = `DELETE FROM funcionarios WHERE id = ${id}`;
+        bdClientes.run(scriptDelete, (e) => {
+          if (!e) {
+            resolve("Registro deletado com sucesso");
+          } else {
+            reject("Não foi possível deletar o registro");
+          }
+        });
+      });
+    } catch (e) {
+      console.error(e.message);
+    }
+  }
+
+  //Método Update
+  async atualizarCliente(
+    id,
+    novoNome,
+    novoGenero,
+    novoData_nascimento,
+    novoCpf,
+    novoTelefone
+  ) {
+    try {
+      return new Promise((resolve, reject) => {
+        const scriptUpdate = `UPDATE clientes SET nome='${novoNome}', genero='${novogenero}', data_nascimento=${novodata_nascimento}, cpf=${novoCpf}, telefone=${novotelefone} WHERE id = ${id}`;
+        bdClientes.run(scriptUpdate, (e) => {
+          if (!e) {
+            resolve("Registro atualizado com sucesso");
+          } else {
+            reject("Não foi possível atualizar o registro");
+          }
+        });
+      });
+    } catch (e) {
+      console.error(e.message);
+    }
+  }
 }
 
-//Método Read
-async buscarTodosClientes(){
-    try{
-        return new Promise((resolve, reject) => {
-            const scriptSelectClientes = `SELECT * FROM clientes`;
-            bdClientes.all(scriptSelectClientes, (e, rows) => {
-                if(!e){
-                    resolve(rows)
-                } else {
-                    reject("Problema ao obter dados")
-                }
-            })
-        })
-    } catch(e){
-        console.error(e.message)
-    }
-}
-
-//Método Read
-getClientesId(id){
-    try{
-        return new Promise((resolve, reject) => {
-            const scriptSelectClienteId = `SELECT * FROM clientes WHERE id = ${id}`;
-            bdClientes.get(scriptSelectClienteId, (e, row) => {
-                if(!e){
-                    if(row != undefined){
-                        resolve(row)
-                    } else {
-                        reject("Nenhum registro encontrado")
-                    }
-                } else {
-                    reject("Não foi possível acessar o banco de dados")
-                }
-            })
-        })
-    } catch(e){
-        console.error(e.message)
-    }
-}
-
-//Método delete
-deleteCliente(id){
-    try{
-        return new Promise((resolve, reject) => {
-            const scriptDelete = `DELETE FROM funcionarios WHERE id = ${id}`;
-            bdClientes.run(scriptDelete, (e) => {
-                if(!e){
-                    resolve("Registro deletado com sucesso")
-                } else {
-                    reject("Não foi possível deletar o registro")
-                }
-            })
-        })
-    } catch(e) {
-        console.error(e.message)
-    }
-}
+export default MetodosClientes;
