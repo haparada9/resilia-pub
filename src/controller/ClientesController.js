907d1c4a48949e247bab902b10a2a6ba6ee1f127
Hugo de Assis Parada <ha.parada9@gmail.com>
2022-03-17 17:45:35 -0300

    atualizando o DELETE e UPDATE

--- src/controller/ClientesController.js
+++ src/controller/ClientesController.js
@@ -48,51 +48,27 @@ class ClientesController{
 
 
 //Método DELETE.
-excluirClientes(req, res){
-   try{
-      const id = parseInt(req.params.id)
-      const script = `DELETE FROM clientes WHERE id = ${id}`
-      bdClientes.run(script, (e) => {
-         if(!e){
-            res.status(200).send("Registro deletado com sucesso")
-         } else {
-            res.status(404).send("Não foi possivel deletar o registro")
-         }
-      })
-   } catch(error){
-      console.error(error)
-   }
+deleteCliente(req, res){
+   const id = req.params.id
+   metodos.deleteCliente(id)
+   .then(response => res.send(response))
+   .catch(response => res.send(response)) 
 }
 
 //Método UPDATE.
-async atualizarCliente(req, res){
-   try{
-      const novoCliente = await new Promise((resolve, reject) => {
-         const result = {
-            nome: req.body.nome,
-            genero: req.body.genero,
-            data_nascimento: req.body.data_nascimento,
-            cpf: parseInt(req.body.cpf),
-            telefone: parseInt(req.body.telefone)
-         }
-         return resolve(result)
-      })
-
-      const id = req.params.id
-
-      const script = `UPDATE clientes SET nome = '${novoCliente.nome}', genero = '${novoCliente.genero}', data_nascimento = ${novoCliente.data_nascimento}, cpf = ${novoCliente.cpf}, telefone = ${novoCliente.telefone} WHERE id = ${id}`
-      
-      bdClientes.run(script, (e) => {
-         if(!e){
-            res.status(200).send("Registro atualizado com sucesso")
-         } else {
-            res.status(404).send("Não foi possivel atualizar o registro")
-            console.log(e)
-         }
-      })
-   } catch(error){
-      console.error(error)
-   }
+async updateCliente(req, res){
+   const novoCliente = await new Promise((resolve, reject) => {         
+      resolve([
+         req.params.id,
+         req.body.nome,
+         req.body.cargo,
+         parseFloat(req.body.salario),
+         parseInt(req.body.cpf)
+      ])
+   })
+   metodos.updateCliente(...novoCliente)
+   .then(response => res.send(response))
+   .catch(response => res.send(response))       
 }
 
 
